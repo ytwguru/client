c75030398ef110c4da24743124d263041e339146
Yomi Toba <ytwguru@gmail.com>
2016-12-05 00:38:23 -0600

    Add hosting and support page

--- src/pages/services/hosting_support/index.jsx
+++ src/pages/services/hosting_support/index.jsx
@@ -17,6 +17,28 @@ export default React.createClass({
       <p>
       We setup production environments, oversee server security, perform daily backups and monitor performance.
       </p>
+      <h3>Hosting</h3>
+      <p>
+      Our team have expertise in working with a variety of Cloud platforms (Amazon Web Services, Azure, Rackspace, and more),
+       migrating local infrastructure to the Cloud, Cloud infrastructure design, etc.  We perform security audits, maintain
+        and manage servers, setup backup storage, update OS and software dependencies, and monitor performance and resource availability. 
+      </p>
+      <h3>Deployment</h3>
+      <p>
+      For launching stable production applications, we setup pre-release / staging environments, implement continuous
+       integration, monitor production servers, and automate scalability.
+      </p>
+      <h3>Support services</h3>
+      <p>
+      We offer 1st, 2nd, and 3rd level support services for application developed by YT Advisors or any third party vendor. 
+       Our team will help you with troubles of any complexity level – from providing workarounds and hot fixes to security
+        audits and application enhancements. 
+      </p>
+      <p>
+        <b>L1 support</b>: user guidance, standard issue fixes, monitor system, data analysis, etc. <br />
+        <b>L2 support</b>: server administration, IT environment support (issue detection and fixing).  Monitoring systems setup<br />
+        <b>L3 support</b>: Source code issue resolution, application code optimization, new functionality etc.
+      </p>
       `
     };
   },
