46a19ddd99652fd26b2a57e3f4f0cce8d50dd973
Yomi Toba <ytwguru@gmail.com>
2016-12-04 22:45:46 -0600

    Add text for technology consulting

--- src/pages/services/technology_consulting/index.jsx
+++ src/pages/services/technology_consulting/index.jsx
@@ -15,6 +15,16 @@ export default React.createClass({
       page : "technology_consulting",
       content : `
       <p>We help teams understand trends, and security risks, and create road maps to reach development goals.</p>
+      <h3>Practical and enduring results</h3>
+      <p>We serve clients at every level of their organization, in whatever capacity we can be most useful,
+        whether as a trusted advisor to top management or as a coach for front line employees. 
+        Our focus is always to deliver practical and enduring results that equip our clients with the tools to grow and lead.
+      </p>
+      <h3>Specialists work directly with our clients</h3>
+      <p>Partnerships with clients lead to recommendations they can put immediately into practice. 
+        Our implementation specialist work directly with our clients to help develop workforce skills,
+        improve operational experience, and apply new working methods.
+      </p>
       `
     };
   },
